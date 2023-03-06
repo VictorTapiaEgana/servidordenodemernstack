6cee82ae4d199e9d054256798e1c98aa6a4d7452
Victor Tapia <victortapiaegana@gmail.com>
2023-03-06 17:29:45 -0600

    se restaura la ruta /api/task

--- index.js
+++ index.js
@@ -1,39 +1,25 @@
-const express = require('express');
-const morgan = require('morgan');
-const path = require('path');
-const cors = require('cors');
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+const cors = require("cors");
 
 const app = express();
-
-const { mongoose } = require('./database')
-
+const { mongoose } = require("./database");
 const port = process.env.PORT || 3001;
-// app.use(PORT);
 
 //Middlewares
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors());
 
-
-
 //routes
-// app.get("/mensaje", (req, res) => {
-//     res.send("Hello");
-// });
+app.use("/api/task", require("./routes/task.routes"));
 
-// app.use('/api/task',require('./routes/task.routes'));
-
-app.use(require('./routes/task.routes'));
+//Starting Server
+app.listen(port, () => {
+  console.log(`Servidor en Puerto ${port}`);
+});
 
 //Static Files
 //  app.use(express.static(path.join(__dirname, '..')));
 //  console.log(path.join(__dirname, '..'));   //BORRAR
-
-//Starting Server
-app.listen(port,()=>{
-    console.log(`Servidor en Puerto ${ port }`) ;
-});
-
-
-
--- routes/task.routes.js
+++ routes/task.routes.js
@@ -1,53 +1,42 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Task = require('../models/task');
+const Task = require("../models/task");
 
 //Tareas Encontradas
-router.get('/', async (req,res) =>{ 
-    console.log(`Servidor en Puerto ${ port }`) ;
-    const tasks = await Task.find();    
-
-    res.json(tasks);
-   
+router.get("/", async (req, res) => {  
+  const tasks = await Task.find();
+  res.json(tasks);
 });
 
 //Listar solo 1
-router.get('/:id', async(req,res)=>{
-
-    const task = await Task.findById(req.params.id);
-    res.json(task);
-
+router.get("/:id", async (req, res) => {
+  const task = await Task.findById(req.params.id);
+  res.json(task);
 });
 
 //Guardar Datos
-router.post('/',async(req,res)=>{
-   
-   console.log(req.body);
-    
-    const { title, description } = req.body;
-   
-    const newTask = new Task({ title,description });
-    await newTask.save();
-      
-    res.json({status:'Tarea Guardada'});
+router.post("/", async (req, res) => {
+  const { title, description } = req.body;
+  const newTask = new Task({ title, description });
+  await newTask.save();
+
+  res.json({ status: "Tarea Guardada" });
 });
 
 //modificar
-router.put('/:id',async(req,res)=>{
+router.put("/:id", async (req, res) => {
+  const { title, description } = req.body;
+  const newTask = { title, description };
+  await Task.findByIdAndUpdate(req.params.id, newTask);
 
-    const { title, description } = req.body;
-    const newTask = ({ title, description });
-    await Task.findByIdAndUpdate(req.params.id,newTask);
-
-    res.json({status:'Tarea Actualizada'});
+  res.json({ status: "Tarea Actualizada" });
 });
 
 //elimimnar
-router.delete('/:id',async(req,res)=>{
+router.delete("/:id", async (req, res) => {
+  await Task.findByIdAndDelete(req.params.id);
 
-    await Task.findByIdAndDelete(req.params.id);
-
-    res.json({status:'Tarea Eliminada'});
+  res.json({ status: "Tarea Eliminada" });
 });
 
-module.exports = router
+module.exports = router;
